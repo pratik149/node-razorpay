e88589b58f12c441c9e582faed15d531a7670e2a
Pratik Rane <pratikrane149@gmail.com>
2021-02-09 07:54:32 +0530

    implement Model schema for PaymentDetail

---
+++ models/payment-detail.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose')
+
+const paymentDetailsSchema = new mongoose.Schema({
+	orderId: {
+		type: String,
+		required: true
+	},
+	receiptId: {
+		type: String
+	},
+	paymentId: {
+		type: String,
+	},
+	signature: {
+		type: String,
+	},
+	amount: {
+		type: Number
+	},
+	currency: {
+		type: String
+	},
+	createdAt: {
+		type: Date
+	},
+	status: {
+		type: String
+	}
+})
+
+module.exports = mongoose.model('PaymentDetail', paymentDetailsSchema)
